abeccd6f7eaed03cf8f234b990ade2731a8fa474
‘JonathanAlbertCross’ <j.a.cross@icloud.com>
2023-10-04 22:00:17 +0100

    tested JSON

--- app2.js
+++ app2.js
@@ -1,6 +1,7 @@
 const img1 = document.getElementById("img1");
 const img2 = document.getElementById("img2");
 const img3 = document.getElementById("img3");
+const img4 = document.getElementById("img4");
 
 // make sure the user only has 25 clicks
 let userClicks = 0;
@@ -62,25 +63,37 @@ function renderProducts() {
   let prod1 = randomProdIdx();
   let prod2 = randomProdIdx();
   let prod3 = randomProdIdx();
+  let prod4 = randomProdIdx();
 
   // make sure they aren't the same
-  while (prod1 === prod2 || prod1 === prod3 || prod2 === prod3) {
+  while (
+    prod1 === prod2 ||
+    prod1 === prod3 ||
+    prod2 === prod3 ||
+    prod1 === prod4 ||
+    prod2 === prod4 ||
+    prod3 === prod4
+  ) {
     prod2 = randomProdIdx();
     prod3 = randomProdIdx();
+    prod4 = randomProdIdx();
   }
 
   // change the src and alt attributes of our img tags
   img1.src = products[prod1].src;
   img2.src = products[prod2].src;
   img3.src = products[prod3].src;
+  img4.src = products[prod4].src;
   img1.alt = products[prod1].name;
   img2.alt = products[prod2].name;
   img3.alt = products[prod3].name;
+  img4.alt = products[prod4].name;
 
   // increase the views of the displayed objects
   products[prod1].views++;
   products[prod2].views++;
   products[prod3].views++;
+  products[prod4].views++;
 }
 
 // handle what happens when click the image
